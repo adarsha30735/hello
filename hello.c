22eba5b68d877fe089d4d671f643fc34455fb4a4
Adarsha Bhattarai <adarsha30735@gmail.com>
2022-01-27 22:44:45 +0000

    added my major

--- hello.c
+++ hello.c
@@ -3,13 +3,13 @@
  * Date: 28 JAN 2022
  * Program to print your name
  */
- 
+
  #include<stdlib.h>
  #include<stdio.h>
- 
+
  int main (int argc, char **argv){
-     
+
      printf(" Adarsha Bhattarai\n");
+     printf("Electrical and computer engineering\n");
      return 0;
  }
- 
