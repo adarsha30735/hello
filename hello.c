1c45360d97d980ba0b794a56d72bc0c6e7c5611e
Adarsha Bhattarai <adarsha30735@gmail.com>
2022-01-27 21:54:13 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Name: Adarsha Bhattarai
+ * Date: 28 JAN 2022
+ * Program to print your name
+ */
+ 
+ #include<stdlib.h>
+ #include<stdio.h>
+ 
+ int main (int argc, char **argv){
+     
+     printf(" Adarsha Bhattarai\n");
+     return 0;
+ }
+ 
